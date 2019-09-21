b26b3966e9a1cc4eedb394c83d2a55f431a283b4
i3629i <50629716+i3629i@users.noreply.github.com>
2019-09-21 15:49:55 +0900

    acos

--- fingerprint_image.py
+++ fingerprint_image.py
@@ -2,7 +2,7 @@ from __future__ import division
 import cv2
 import numpy as np
 import time
-
+import math
 
 protoFile = "Model/pose_deploy.prototxt"
 weightsFile = "Model/pose_iter_102000.caffemodel"
@@ -11,7 +11,7 @@ nPoints = 22
 POSE_PAIRS = [ [0,1],[1,2],[2,3],[3,4],[0,5],[5,6],[6,7],[7,8],[0,9],[9,10],[10,11],[11,12],[0,13],[13,14],[14,15],[15,16],[0,17],[17,18],[18,19],[19,20] ]
 net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 
-frame = cv2.imread("Image/42.jpg")
+frame = cv2.imread("Image/03.jpg")
 frameCopy = np.copy(frame)
 frameWidth = frame.shape[1]
 frameHeight = frame.shape[0]
@@ -48,9 +48,6 @@ for i in range(nPoints):
     else :
         points.append(None)
 
-
-
-
 for pair in POSE_PAIRS:
     partA = pair[0]
     partB = pair[1]
@@ -64,20 +61,42 @@ for pair in POSE_PAIRS:
 
 finger = [points[20],points[16],points[12],points[8],points[4]]
 finger_second = [points[19],points[15],points[11],points[7],points[3]]
+
+if None in finger:
+    finger.remove(None)
+
+if None in finger_second:
+    finger_second.remove(None)
+
+
 print(finger)
 print(finger_second)
 
 
+
 for i in range(len(finger)):
-    if finger and finger_second is None:
-        break
-    else:
-        fingerprint_X = int((finger[i][0] + finger_second[i][0]) / 2)
-        fingerprint_Y = int((finger[i][1] + finger_second[i][1]) / 2)
 
-        cv2.circle(frame, (fingerprint_X, fingerprint_Y), 20, (255, 255, 0), thickness=1)
+    fingerprint_X = int((finger[i][0] + finger_second[i][0]) / 2)
+    fingerprint_Y = int((finger[i][1] + finger_second[i][1]) / 2)
+    print(fingerprint_X,fingerprint_Y)
+    # circle_size = finger[i][0] - fingerprint_X +  fingerprint_Y - finger[i][1]
+    # print(circle_size)
+    # cv2.circle(frame, (fingerprint_X, fingerprint_Y), circle_size, (255, 255, 0), thickness=1)
+    first_X =finger[i][0]
+    first_Y = finger[i][1]
+    second_X = finger_second[i][0]
+    second_Y = finger_second[i][1]
+
+    X = first_X - second_X
+    Y = first_Y - second_Y
 
 
+    B = math.sqrt(math.pow(X,2) + math.pow(Y,2))
+    print(B)
+    acos = math.acos( X/B ) * 57.3
+    print('acos : ',acos)
+   # cos = math.acos()
+    cv2.ellipse(frame,(fingerprint_X, fingerprint_Y),(50,20),-acos,0,360,(255,255,0),1)
 
 # cv2.circle(frame,finger[1])
 
@@ -85,7 +104,7 @@ for i in range(len(finger)):
 # mask_hand = cv2.inRange(ycrcb, np.array([0,133,77]), np.array([255,173,127]))
 
 # cv2.imshow('hands',mask_hand)
-frame = cv2.resize(frame,(480,680))
+# frame = cv2.resize(frame,(480,680))
 # cv2.imshow('Output-Keypoints', frameCopy)
 
 cv2.imshow('Output', frame)
